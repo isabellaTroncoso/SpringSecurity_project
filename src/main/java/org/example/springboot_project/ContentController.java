b7f6ee59d930b66bc6dd63ecf802f4d8e6830d7e
isabellaTroncoso <isabella.troncoso@stud.sti.se>
2025-06-03 10:25:03 +0200

    add classes

---
+++ src/main/java/org/example/springboot_project/ContentController.java
@@ -0,0 +1,22 @@
+package org.example.springboot_project;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+@Controller
+public class ContentController {
+    @GetMapping("/home")
+    public String home(){
+        return "home";
+    }
+
+    @GetMapping("/admin/home")
+    public String adminHome(){
+        return "admin";
+    }
+
+    @GetMapping("/user/home")
+    public String userHome(){
+        return "user";
+    }
+
+}
